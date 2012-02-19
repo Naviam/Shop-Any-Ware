eeca27a491430833b0c5430bc34d3f85175954f0
Vitali Hatalski <v.hatalski@gmail.com>
2012-02-20 00:59:37 +0300

    make member home page default

--- TdService/Global.asax.cs
+++ TdService/Global.asax.cs
@@ -44,7 +44,7 @@ namespace TdService.Web
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Account", action = "SignIn", id = UrlParameter.Optional }); // Parameter defaults
+                new { controller = "Member", action = "Home", id = UrlParameter.Optional }); // Parameter defaults
         }
 
         /// <summary>
