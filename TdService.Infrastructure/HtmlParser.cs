06bcbe33f725e4e4f003a7ac09a5e4f58494fe46
Vitali Hatalski <v.hatalski@gmail.com>
2012-03-01 00:49:29 +0300

    update

---
+++ TdService.Infrastructure/HtmlParser.cs
@@ -0,0 +1,60 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HtmlParser.cs" company="TdService">
+// Vitali Hatalski. 2012.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TdService.Infrastructure
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using HtmlAgilityPack;
+    using ScrapySharp.Extensions;
+
+    /// <summary>
+    /// This class is used to parse html pages to get some data from them.
+    /// </summary>
+    public class HtmlParser
+    {
+        /// <summary>
+        /// Extract retailers from HTML file.
+        /// </summary>
+        /// <param name="content">
+        /// The content.
+        /// </param>
+        /// <returns>
+        /// Collection of the retailers.
+        /// </returns>
+        public IEnumerable<Retailer> GetOnlineRetailers(StreamReader content)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.Load(content);
+            var html = htmlDocument.DocumentNode;
+
+            var retailersHtml = html.CssSelect("#company_listing").CssSelect("div");
+            return retailersHtml.Where(retailer => retailer.HasChildNodes)
+                .Select(retailerHtml => new Retailer
+                {
+                    Name = retailerHtml.CssSelect("p[class='company_name'] > a").First().InnerText,
+                    Category = retailerHtml.CssSelect("p[class='company_category']").First().InnerText
+                });
+        }
+
+        /// <summary>
+        /// The class contains retailer details.
+        /// </summary>
+        public class Retailer
+        {
+            /// <summary>
+            /// Gets or sets Name.
+            /// </summary>
+            public string Name { get; set; }
+
+            /// <summary>
+            /// Gets or sets Category.
+            /// </summary>
+            public string Category { get; set; }
+        }
+    }
+}
---
+++ TdService.UnitTests/HtmlParserTestSuite.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HtmlParserTestSuite.cs" company="TdService">
+//   Vitali Hatalski. 2012
+// </copyright>
+// <summary>
+//   Defines the HtmlParserTestSuite type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TdService.UnitTests
+{
+    using System.IO;
+    using Infrastructure;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+
+    /// <summary>
+    /// Test suite for htmlparser class.
+    /// </summary>
+    [TestClass]
+    public class HtmlParserTestSuite
+    {
+        /// <summary>
+        /// Get retailers test.
+        /// </summary>
+        [TestMethod]
+        public void GetRetailersTest()
+        {
+            // arrange
+            var stream = File.OpenRead("RawHtml/top100.htm");
+            var parser = new HtmlParser();
+            const int ExpectedCount = 100;
+
+            // act
+            var retailers = parser.GetOnlineRetailers(new StreamReader(stream)).ToList();
+
+            // assert
+            Assert.AreEqual(ExpectedCount, retailers.Count);
+        }
+    }
+}
