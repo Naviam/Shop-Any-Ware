33c53e5037509a644b66f601af7fa223b0ac857f
Vitali Hatalski <v.hatalski@gmail.com>
2012-03-21 14:41:44 +0300

    packages

---
+++ TdService.Model/Packages/IParcelRepository.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IPackageRepository.cs" company="TdService">
+//   Vitali Hatalski. 2012.
+// </copyright>
+// <summary>
+//   Defines the IPackageRepository type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TdService.Model.Packages
+{
+    using System.Collections.Generic;
+    using Domain;
+    using Orders;
+
+    /// <summary>
+    /// The interface for package repository.
+    /// </summary>
+    public interface IPackageRepository 
+    {
+        void AddOrdersToParcel(int parcelId, IEnumerable<Order> orders);
+
+        void RemoveParcel(int parcelId);
+
+        IEnumerable<Parcel> GetUserParcels(string username, object sortDirection, string sortExpression, string filterExpression, int pageSize);
+
+        IEnumerable<Parcel> GetAllParcels(object sortDirection, string sortExpression, string filterExpression, int pageSize = 20);
+
+        Parcel GetParcelDetails(int parcelId);
+
+        void UpdateParcel(Parcel parcel);
+
+        void UpdateParcelStatus(int parcelId, ParcelStatus newStatus);
+
+        /// <summary>
+        /// Adds new parcel and returns parcel id
+        /// </summary>
+        int AddParcel(string username, Parcel parcel);
+    }
+}
---
+++ TdService.Model/Packages/Parcel.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Parcel.cs" company="TdService">
+//   Vitali Hatalski. 2012.
+// </copyright>
+// <summary>
+//   Defines the Parcel type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TdService.Model.Packages
+{
+    using System.Collections.Generic;
+    using Domain;
+    using Orders;
+
+    /// <summary>
+    /// Parcel that user creates to send to home address.
+    /// </summary>
+    public class Parcel
+    {
+        /// <summary>
+        /// Gets or sets Id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets Name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets Address.
+        /// </summary>
+        public Address Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets Orders.
+        /// </summary>
+        public virtual IEnumerable<Order> Orders { get; set; }
+
+        /// <summary>
+        /// Gets or sets DispatchMethod.
+        /// </summary>
+        public virtual DispatchMethod DispatchMethod { get; set; }
+
+        /// <summary>
+        /// Gets or sets Services.
+        /// </summary>
+        public virtual IEnumerable<Service> Services { get; set; }
+    }
+}
