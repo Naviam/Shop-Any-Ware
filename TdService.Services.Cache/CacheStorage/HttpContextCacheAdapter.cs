35d0707de13246a37a53f38e8f201699cd53d22b
Vitali Hatalski <v.hatalski@gmail.com>
2012-05-31 14:56:52 +0300

    cache project

---
+++ TdService.Services.Cache/CacheStorage/HttpContextCacheAdapter.cs
@@ -0,0 +1,58 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HttpContextCacheAdapter.cs" company="TdService">
+// Vitali Hatalski. 2012.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TdService.Services.Cache.CacheStorage
+{
+    using System.Web;
+
+    /// <summary>
+    /// This is a wrapper on the default asp.net cache in memory provider.
+    /// </summary>
+    public class HttpContextCacheAdapter
+    {
+        /// <summary>
+        /// Remove object from cache storage by key.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        public void Remove(string key)
+        {
+            HttpContext.Current.Cache.Remove(key);
+        }
+
+        /// <summary>
+        /// Save object to cache storage with unique key.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <param name="data">
+        /// The data.
+        /// </param>
+        public void Store(string key, object data)
+        {
+            HttpContext.Current.Cache.Insert(key, data);
+        }
+
+        /// <summary>
+        /// Get object from cache storage by key.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <typeparam name="T">
+        /// Custom class type of an object.
+        /// </typeparam>
+        /// <returns>
+        /// Object of a custom class type.
+        /// </returns>
+        public T Retrieve<T>(string key) where T : class
+        {
+            return (T)HttpContext.Current.Cache.Get(key);
+        }
+    }
+}
---
+++ TdService.Services.Cache/CacheStorage/ICacheStorage.cs
@@ -0,0 +1,47 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ICacheStorage.cs" company="TdService">
+// Vitali Hatalski. 2012.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TdService.Services.Cache.CacheStorage
+{
+    /// <summary>
+    /// Interface for cache storage.
+    /// </summary>
+    public interface ICacheStorage
+    {
+        /// <summary>
+        /// Remove object from cache storage by key.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        void Remove(string key);
+
+        /// <summary>
+        /// Save object with unique key to cache storage.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <param name="data">
+        /// The data.
+        /// </param>
+        void Store(string key, object data);
+
+        /// <summary>
+        /// Get object from cache storage by key.
+        /// </summary>
+        /// <param name="storageKey">
+        /// The storage key.
+        /// </param>
+        /// <typeparam name="T">
+        /// Any class type.
+        /// </typeparam>
+        /// <returns>
+        /// Object of custom type.
+        /// </returns>
+        T Retrieve<T>(string storageKey);
+    }
+}
