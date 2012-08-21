fc3d22d0c5ed1274ba352c2b8cd0a3b1b8f19c6c
Vitali Hatalski <v.hatalski@gmail.com>
2012-08-21 18:15:08 +0300

    added knockout models for signup and signin

---
+++ TdService.UI.Web.MVC/Scripts/ViewModels/SignInViewModel.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../jquery-1.7.2-vsdoc.js" />
+/// <reference path="../jquery.noty.js" />
+/// <reference path="../json2.js" />
+/// <reference path="../knockout-2.1.0.debug.js" />
+/// <reference path="../knockout.mapping-latest.debug.js" />
+/// <reference path="../knockout.validation.debug.js" />
+/// <reference path="../knockout-sortable.js" />
+/// <reference path="../bootstrap/bootstrap-collapse.js" />
+
+function SignInViewModel(serverModel) {
+    var self = this;
+
+    self.email = ko.observable('');
+    self.password = ko.observable('');
+}
---
+++ TdService.UI.Web.MVC/Scripts/ViewModels/SignUpViewModel.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../jquery-1.7.2-vsdoc.js" />
+/// <reference path="../jquery.noty.js" />
+/// <reference path="../json2.js" />
+/// <reference path="../knockout-2.1.0.debug.js" />
+/// <reference path="../knockout.mapping-latest.debug.js" />
+/// <reference path="../knockout.validation.debug.js" />
+/// <reference path="../knockout-sortable.js" />
+/// <reference path="../bootstrap/bootstrap-collapse.js" />
+
+function SignUpViewModel(serverModel) {
+    var self = this;
+
+    self.email = ko.observable('');
+    self.password = ko.observable('');
+    self.passwordConfirm = ko.observable('');
+    self.firstName = ko.observable('');
+    self.lastName = ko.observable('');
+}
