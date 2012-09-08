8a1e9191486a85605b13f1d19d808411423b08ad
Vitali Hatalski <v.hatalski@gmail.com>
2012-09-08 14:40:33 +0300

    replaced combres config with bundle

--- TdService.UI.Web.MVC/App_Start/BundleConfig.cs
+++ TdService.UI.Web.MVC/App_Start/BundleConfig.cs
@@ -7,37 +7,55 @@
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            var b = new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js");
+            bundles.Add(new StyleBundle("~/Content/sitecss").Include(
+                        "~/Content/less/bootstrap.css",
+                        "~/Content/less/responsive.css",
+                        "~/Content/styles.css",
+                        "~/Content/noty/jquery.noty.css",
+                        "~/Content/noty/noty_theme_default.css",
+                        "~/Content/noty/noty_theme_twitter.css"));
 
-            bundles.Add(b);
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/json2.js",
+                        "~/scripts/linq.min.js",
+                        "~/scripts/jquery.validate.min.js",
+                        "~/scripts/jquery.validate.unobtrusive.min.js",
+                        "~/scripts/ui/jquery-ui-1.8.18.custom.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap/bootstrap-transition.js",
+                        "~/Scripts/bootstrap/bootstrap-alert.js",
+                        "~/Scripts/bootstrap/bootstrap-modal.js",
+                        "~/Scripts/bootstrap/bootstrap-dropdown.js",
+                        "~/Scripts/bootstrap/bootstrap-scrollspy.js",
+                        "~/Scripts/bootstrap/bootstrap-tab.js",
+                        "~/Scripts/bootstrap/bootstrap-tooltip.js",
+                        "~/Scripts/bootstrap/bootstrap-popover.js",
+                        "~/Scripts/bootstrap/bootstrap-button.js",
+                        "~/Scripts/bootstrap/bootstrap-collapse.js",
+                        "~/Scripts/bootstrap/bootstrap-carousel.js",
+                        "~/Scripts/bootstrap/bootstrap-typeahead.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/noty").Include(
+                        "~/Scripts/noty/jquery.noty.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                        "~/Scripts/knockout-2.1.0.js",
+                        "~/Scripts/knockout.mapping-latest.js",
+                        "~/Scripts/knockout.validation.js",
+                        "~/Scripts/knockout-sortable.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/common").Include(
+                        "~/Scripts/Common.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dashboard").Include(
+                        "~/Scripts/ViewModels/DashboardViewModel.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            // bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
         }
     }
 }
