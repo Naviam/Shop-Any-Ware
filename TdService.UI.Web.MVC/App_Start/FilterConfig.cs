a4f3b7ba2c353f41a39bada5f1c66877172004fe
Vitali Hatalski <v.hatalski@gmail.com>
2012-09-08 13:42:32 +0300

    updated app config

---
+++ TdService.UI.Web.MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace TdService.UI.Web.MVC.App_Start
+{
+    using System.Web.Mvc;
+
+    public class FilterConfig
+    {
+        /// <summary>
+        /// Register global filters such as error handlers
+        /// </summary>
+        /// <param name="filters">
+        /// The filters.
+        /// </param>
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
--- TdService.UI.Web.MVC/Global.asax.cs
+++ TdService.UI.Web.MVC/Global.asax.cs
@@ -10,9 +10,11 @@
 namespace TdService.UI.Web.MVC
 {
     using System.Web.Mvc;
+    using System.Web.Optimization;
     using System.Web.Routing;
 
     using TdService.Repository.MsSql;
+    using TdService.UI.Web.MVC.App_Start;
 
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
@@ -23,36 +25,6 @@ namespace TdService.UI.Web.MVC
     public class MvcApplication : System.Web.HttpApplication
     {
         /// <summary>
-        /// Register global filters such as error handlers
-        /// </summary>
-        /// <param name="filters">
-        /// The filters.
-        /// </param>
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
-
-        /// <summary>
-        /// Register routes
-        /// </summary>
-        /// <param name="routes">
-        /// The routes.
-        /// </param>
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional }); // Parameter defaults
-
-            routes.MapRoute("Welcome", "{action}", new { controller = "Member", action = "Welcome" });
-            routes.MapRoute("Dashboard", "{action}", new { controller = "Member", action = "Dashboard" });
-        }
-
-        /// <summary>
         /// Application start even handler
         /// </summary>
         protected void Application_Start()
@@ -62,8 +34,9 @@ namespace TdService.UI.Web.MVC
             DatabaseInitializer.InitializeShopAnyWare();
 
             // AreaRegistration.RegisterAllAreas();
-            RegisterGlobalFilters(GlobalFilters.Filters);
-            RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
