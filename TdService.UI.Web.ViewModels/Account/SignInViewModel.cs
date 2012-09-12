858f2784903df2cdb31ad7be100761b14fdd5272
Vitali Hatalski <v.hatalski@gmail.com>
2012-09-12 15:05:53 +0300

    removed annotationsfrom sign and singup view models

--- TdService.UI.Web.ViewModels/Account/SignInViewModel.cs
+++ TdService.UI.Web.ViewModels/Account/SignInViewModel.cs
@@ -6,8 +6,6 @@
 
 namespace TdService.UI.Web.ViewModels.Account
 {
-    using System.ComponentModel.DataAnnotations;
-
     /// <summary>
     /// This class describes the view model for sign in page.
     /// </summary>
@@ -16,18 +14,11 @@ namespace TdService.UI.Web.ViewModels.Account
         /// <summary>
         /// Gets or sets Email.
         /// </summary>
-        [Required(ErrorMessageResourceName = "RequiredEmail", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
-        [StringLength(256, ErrorMessageResourceName = "MaxLengthEmail", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
-        [RegularExpression(@"^[\w\.\-]+@[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]{1,})*(\.[a-zA-Z]{2,3}){1,2}$",
-            ErrorMessageResourceName = "InvalidEmail",
-            ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
         public string Email { get; set; }
 
         /// <summary>
         /// Gets or sets Password.
         /// </summary>
-        [Required(ErrorMessageResourceName = "RequiredPassword", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
-        [StringLength(21, ErrorMessageResourceName = "MaxLengthPassword", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
         public string Password { get; set; }
 
         /// <summary>
--- TdService.UI.Web.ViewModels/Account/SignUpViewModel.cs
+++ TdService.UI.Web.ViewModels/Account/SignUpViewModel.cs
@@ -6,8 +6,6 @@
 
 namespace TdService.UI.Web.ViewModels.Account
 {
-    using System.ComponentModel.DataAnnotations;
-
     /// <summary>
     /// This class describes the view model for sign up page.
     /// </summary>
@@ -16,39 +14,26 @@ namespace TdService.UI.Web.ViewModels.Account
         /// <summary>
         /// Gets or sets Email.
         /// </summary>
-        [Required(ErrorMessageResourceName = "UserEmailRequired", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
-        [StringLength(256, ErrorMessageResourceName = "UserEmailMaxLength", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
-        [RegularExpression(@"^[\w\.\-]+@[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]{1,})*(\.[a-zA-Z]{2,3}){1,2}$",
-            ErrorMessageResourceName = "UserEmailInvalid",
-            ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
         public string Email { get; set; }
 
         /// <summary>
         /// Gets or sets Password.
         /// </summary>
-        [Required(ErrorMessageResourceName = "UserPasswordRequired", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
-        [StringLength(21, MinimumLength = 7, ErrorMessageResourceName = "UserPasswordMaxLength", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
         public string Password { get; set; }
 
         /// <summary>
         /// Gets or sets Password Confirm.
         /// </summary>
-        [Required(ErrorMessageResourceName = "UserPasswordConfirmRequired", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
-        [Compare("Password", ErrorMessageResourceName = "UserPasswordConfirmNotEqual", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
         public string PasswordConfirm { get; set; }
 
         /// <summary>
         /// Gets or sets First Name.
         /// </summary>
-        [Required(ErrorMessageResourceName = "ProfileFirstNameRequired", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
-        [StringLength(64, ErrorMessageResourceName = "ProfileFirstNameMaxLength", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
         public string FirstName { get; set; }
 
         /// <summary>
         /// Gets or sets Last Name.
         /// </summary>
-        [Required(ErrorMessageResourceName = "ProfileLastNameRequired", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
-        [StringLength(64, ErrorMessageResourceName = "ProfileLastNameMaxLength", ErrorMessageResourceType = typeof(Resources.ErrorCodeResources))]
         public string LastName { get; set; }
 
         /// <summary>
