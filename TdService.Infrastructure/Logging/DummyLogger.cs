b8f15f062e6997e3119a521fcde0bab07f5467d9
Vitali Hatalski <v.hatalski@gmail.com>
2012-09-13 01:56:02 +0300

    added dummy logger
    so logger interface can be used right now, the implementation for amazon
    dynamo db will follows

---
+++ TdService.Infrastructure/Logging/DummyLogger.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DummyLogger.cs" company="TdService">
+//   Vitali Hatalski. 2012.
+// </copyright>
+// <summary>
+//   The dummy logger.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TdService.Infrastructure.Logging
+{
+    using System;
+
+    /// <summary>
+    /// The dummy logger.
+    /// </summary>
+    public class DummyLogger : ILogger
+    {
+        /// <summary>
+        /// Log a message.
+        /// </summary>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        public void Log(string message)
+        {
+        }
+
+        /// <summary>
+        /// Log the error.
+        /// </summary>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        /// <param name="exception">
+        /// The exception.
+        /// </param>
+        public void Error(string message, Exception exception)
+        {
+        }
+    }
+}
