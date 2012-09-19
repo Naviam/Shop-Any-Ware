b2ed6feef6b4cad354567c38236bf6ffe940cfd1
Vitali Hatalski <v.hatalski@gmail.com>
2012-09-19 11:29:46 +0300

    added mvc 4 project

---
+++ TdService.UI.Web.MVC4/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WebApiConfig.cs" company="TdService">
+//   Vitali Hatalski. 2012.
+// </copyright>
+// <summary>
+//   The web api config.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TdService.UI.Web.MVC4.App_Start
+{
+    using System.Web.Http;
+
+    /// <summary>
+    /// The web api config.
+    /// </summary>
+    public static class WebApiConfig
+    {
+        /// <summary>
+        /// The register.
+        /// </summary>
+        /// <param name="config">
+        /// The config.
+        /// </param>
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+        }
+    }
+}
