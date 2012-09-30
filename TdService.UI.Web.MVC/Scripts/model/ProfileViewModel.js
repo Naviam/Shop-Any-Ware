0b77da855a22c3225069584e8eaa603c0e47cb65
Vitali Hatalski <v.hatalski@gmail.com>
2012-09-30 19:36:55 +0300

    fixed profile page

--- TdService.UI.Web.MVC/Scripts/model/ProfileViewModel.js
+++ TdService.UI.Web.MVC/Scripts/model/ProfileViewModel.js
@@ -11,28 +11,15 @@
     self.currentPassword = ko.observable(profile.CurrentPassword);
     self.firstName = ko.observable(profile.FirstName);
     self.lastName = ko.observable(profile.LastName);
-    self.notifyOnOrderStatusChange = ko.observable(profile.NotifyOnOrderStatusChange);
-    self.notifyOnPackageStatusChange = ko.observable(profile.NotifyOnPackageStatusChange);
+    self.notifyOnOrderStatusChanged = ko.observable(profile.NotifyOnOrderStatusChanged);
+    self.notifyOnPackageStatusChanged = ko.observable(profile.NotifyOnPackageStatusChanged);
 
     self.updateProfile = function () {
         /// <summary>Update first and last name.</summary>
         if ($("form").valid()) {
             $.post("Profile/Save", $("form").serialize(),
-                function(data, textStatus, jqXhr) {
-                    noty({
-                        "text": data.Message,
-                        "layout": "topCenter",
-                        "type": data.MessageType,
-                        "theme": "noty_theme_twitter",
-                        "animateOpen": { "height": "toggle" },
-                        "animateClose": { "height": "toggle" },
-                        "speed": 500,
-                        "timeout": 8000,
-                        "closeButton": true,
-                        "closeOnSelfClick": true,
-                        "closeOnSelfOver": false,
-                        "modal": false
-                    });
+                function (data) {
+                    window.showNotice(data.Message, data.MessageType);
                 });
         }
     };
