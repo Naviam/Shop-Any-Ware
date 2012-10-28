d1dfd5d39ab2705bdaf1ee84c4b68dca91f0bb34
Vitali Hatalski <v.hatalski@gmail.com>
2012-10-29 02:43:26 +0300

    clean db init

--- TdService.ShopAnyWare.Specs/ShopAnyWareTestInitilizer.cs
+++ TdService.ShopAnyWare.Specs/ShopAnyWareTestInitilizer.cs
@@ -14,29 +14,9 @@ namespace TdService.ShopAnyWare.Specs
     using TdService.Repository.MsSql;
 
     /// <summary>
-    /// Shop any ware database initilizer that drops and recreate database for running db tests.
+    /// Shop any ware database initialization that drops and recreate database for running DB tests.
     /// </summary>
     public class ShopAnyWareTestInitilizer : DropCreateDatabaseAlways<ShopAnyWareSql>
     {
-        /// <summary>
-        /// Populate database with static data.
-        /// </summary>
-        /// <param name="context">
-        /// The context.
-        /// </param>
-        protected override void Seed(ShopAnyWareSql context)
-        {
-            ////SeedMembership.Populate(context);
-            ////SeedCurrencies.Populate(context);
-
-            //////// SeedRetailers.Populate(context, new FileStorage());
-            ////context.Retailers.Add(new Retailer("amazon.com"));
-            ////context.Retailers.Add(new Retailer("apple.com"));
-            ////context.SaveChanges();
-            ////SeedOrders.Populate(context);
-            ////SeedPackages.Populate(context);
-
-            base.Seed(context);
-        }
     }
 }
