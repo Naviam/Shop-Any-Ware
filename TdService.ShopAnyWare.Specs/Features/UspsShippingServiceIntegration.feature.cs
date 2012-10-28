0b7c8ef44a46dbdb7c89bf2bca265dc59b14a5ae
Vitali Hatalski <v.hatalski@gmail.com>
2012-10-28 14:00:22 +0300

    updates

--- TdService.ShopAnyWare.Specs/Features/UspsShippingServiceIntegration.feature.cs
+++ TdService.ShopAnyWare.Specs/Features/UspsShippingServiceIntegration.feature.cs
@@ -11,7 +11,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace TdService.Specs.Features
+namespace TdService.ShopAnyWare.Specs.Features
 {
     using TechTalk.SpecFlow;
     
--- TdService.ShopAnyWare.Specs/Properties/AssemblyInfo.cs
+++ TdService.ShopAnyWare.Specs/Properties/AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TdService.Specs")]
+[assembly: AssemblyTitle("TdService.ShopAnyWare.Specs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TdService.Specs")]
+[assembly: AssemblyProduct("TdService.ShopAnyWare.Specs")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
