8823cb7e20f1a3bb7dba2f78cabd6be16e479252
Vitali Hatalski <v.hatalski@gmail.com>
2012-11-13 14:23:43 +0300

    small fixes

--- TdService.Services/Implementations/ItemsService.cs
+++ TdService.Services/Implementations/ItemsService.cs
@@ -13,7 +13,6 @@ namespace TdService.Services.Implementations
     using System.Linq;
     using System.Text;
 
-    using TdService.Model.Common;
     using TdService.Model.Items;
     using TdService.Model.Packages;
     using TdService.Services.Interfaces;
