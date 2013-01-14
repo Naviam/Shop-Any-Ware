2689feaca42ab9422e1a844d81b12920fbef6c61
Vitali Hatalski <v.hatalski@gmail.com>
2013-01-14 13:46:42 +0300

    update

--- TdService.Infrastructure/Logging/NLogAmazonDynamoDbTarget.cs
+++ TdService.Infrastructure/Logging/NLogAmazonDynamoDbTarget.cs
@@ -1,14 +1,15 @@
 ﻿namespace TdService.Infrastructure.Logging
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Amazon;
-    using Amazon.DynamoDB.DocumentModel;
-    using NLog;
-    using NLog.Targets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDB.DocumentModel;
+using NLog;
+using NLog.Common;
+using NLog.Targets;
 
     [Target("DynamoDb")]
     public class NLogAmazonDynamoDbTarget : TargetWithLayout
@@ -22,38 +23,42 @@
 
         public string AmazonSecretAccessKey { get; set; }
 
-        protected override void Write(NLog.Common.AsyncLogEventInfo logEvent)
+        private Document CreateLogDocument(LogEventInfo logEvent)
+        {
+            var log = new Document();
+            log["Id"] = logEvent.SequenceID;
+            log["TimeStamp"] = logEvent.TimeStamp;
+            log["Level"] = logEvent.Level.ToString();
+            log["Message"] = logEvent.Message;
+            log["UserStackFrameNumber"] = logEvent.UserStackFrameNumber;
+            if (logEvent.UserStackFrame != null)
+            {
+                log["FileName"] = logEvent.UserStackFrame.GetFileName();
+                log["FileLineNumber"] = logEvent.UserStackFrame.GetFileLineNumber();
+                log["FileColumnNumber"] = logEvent.UserStackFrame.GetFileColumnNumber();
+                log["MethodName"] = logEvent.UserStackFrame.GetMethod().Name;
+            }
+
+            if (logEvent.Exception != null)
+            {
+                log["ExceptionMessage"] = logEvent.Exception.Message;
+                log["ExceptionHResult"] = logEvent.Exception.HResult;
+                log["ExceptionStackTrace"] = logEvent.Exception.StackTrace;
+                log["ExceptionSource"] = logEvent.Exception.Source;
+            }
+
+            log["UserEmail"] = logEvent.Properties["UserEmail"].ToString();
+            log["OrderId"] = logEvent.Properties["OrderId"].ToString();
+            log["PackageId"] = logEvent.Properties["PackageId"].ToString();
+            log["TransactionId"] = logEvent.Properties["TransactionId"].ToString();
+        }
+
+        protected override void Write(AsyncLogEventInfo logEvent)
         {
             using (var client = AWSClientFactory.CreateAmazonDynamoDBClient(this.AmazonAccessKeyId, this.AmazonSecretAccessKey))
             {
                 Table table = Table.LoadTable("TdService_Logs");
-                var log = new Document();
-                log["Id"] = logEvent.SequenceID;
-                log["TimeStamp"] = logEvent.TimeStamp;
-                log["Level"] = logEvent.Level.ToString();
-                log["Message"] = logEvent.Message;
-                log["UserStackFrameNumber"] = logEvent.UserStackFrameNumber;
-                if (logEvent.UserStackFrame != null)
-                {
-                    log["FileName"] = logEvent.UserStackFrame.GetFileName();
-                    log["FileLineNumber"] = logEvent.UserStackFrame.GetFileLineNumber();
-                    log["FileColumnNumber"] = logEvent.UserStackFrame.GetFileColumnNumber();
-                    log["MethodName"] = logEvent.UserStackFrame.GetMethod().Name;
-                }
-
-                if (logEvent.Exception != null)
-                {
-                    log["ExceptionMessage"] = logEvent.Exception.Message;
-                    log["ExceptionHResult"] = logEvent.Exception.HResult;
-                    log["ExceptionStackTrace"] = logEvent.Exception.StackTrace;
-                    log["ExceptionSource"] = logEvent.Exception.Source;
-                }
-
-                log["UserEmail"] = logEvent.Properties["UserEmail"].ToString();
-                log["OrderId"] = logEvent.Properties["OrderId"].ToString();
-                log["PackageId"] = logEvent.Properties["PackageId"].ToString();
-                log["TransactionId"] = logEvent.Properties["TransactionId"].ToString();
-
+                var log = CreateLogDocument(logEvent.LogEvent);
                 table.BeginPutItem(log, );
             }
 
@@ -65,33 +70,7 @@
             using (var client = AWSClientFactory.CreateAmazonDynamoDBClient(this.AmazonAccessKeyId, this.AmazonSecretAccessKey))
             {
                 Table table = Table.LoadTable("TdService_Logs");
-                var log = new Document();
-                log["Id"] = logEvent.SequenceID;
-                log["TimeStamp"] = logEvent.TimeStamp;
-                log["Level"] = logEvent.Level.ToString();
-                log["Message"] = logEvent.Message;
-                log["UserStackFrameNumber"] = logEvent.UserStackFrameNumber;
-                if (logEvent.UserStackFrame != null)
-                {
-                    log["FileName"] = logEvent.UserStackFrame.GetFileName();
-                    log["FileLineNumber"] = logEvent.UserStackFrame.GetFileLineNumber();
-                    log["FileColumnNumber"] = logEvent.UserStackFrame.GetFileColumnNumber();
-                    log["MethodName"] = logEvent.UserStackFrame.GetMethod().Name;
-                }
-
-                if (logEvent.Exception != null)
-                {
-                    log["ExceptionMessage"] = logEvent.Exception.Message;
-                    log["ExceptionHResult"] = logEvent.Exception.HResult;
-                    log["ExceptionStackTrace"] = logEvent.Exception.StackTrace;
-                    log["ExceptionSource"] = logEvent.Exception.Source;
-                }
-
-                log["UserEmail"] = logEvent.Properties["UserEmail"].ToString();
-                log["OrderId"] = logEvent.Properties["OrderId"].ToString();
-                log["PackageId"] = logEvent.Properties["PackageId"].ToString();
-                log["TransactionId"] = logEvent.Properties["TransactionId"].ToString();
-
+                var log = CreateLogDocument(logEvent);
                 table.PutItem(log);
             }
 
