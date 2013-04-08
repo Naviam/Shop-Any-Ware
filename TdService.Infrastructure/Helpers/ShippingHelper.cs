568cc43f99d9ccfcca3c6f9d92c22a7bdc3ff320
Vitali Hatalski <v.hatalski@gmail.com>
2013-04-08 18:53:17 -0400

    added shipping api wrappers

---
+++ TdService.Infrastructure/Helpers/ShippingHelper.cs
@@ -0,0 +1,67 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ShippingHelper.cs" company="TdService">
+//   Vitali Hatalski. 2012.
+// </copyright>
+// <summary>
+//   The shipping helper.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TdService.Infrastructure.Helpers
+{
+    using DotNetShipping;
+    using DotNetShipping.ShippingProviders;
+
+    using SeeSharpShip.Models.Usps.International.Request;
+    using SeeSharpShip.Services.Usps;
+
+    /// <summary>
+    /// The shipping helper.
+    /// </summary>
+    public class ShippingHelper
+    {
+        /// <summary>
+        /// The get shipping rate.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="decimal"/>.
+        /// </returns>
+        public decimal GetShippingRate()
+        {
+            // You will need a userId to use the USPS provider. Your account will also need access to the production servers.
+            const string UspsUserId = "852TRADE0543";
+
+            var origin = new Address(string.Empty, string.Empty, "06405", "US");
+            var destination = new Address(string.Empty, string.Empty, "121352", "RU"); // US Address
+
+            // Create RateManager
+            var rateManager = new RateManager();
+            var provider = new USPSProvider(UspsUserId);
+
+            rateManager.AddProvider(provider);
+
+            // Call GetRates()
+            var shipment = rateManager.GetRates(origin, destination, new Package(12, 12, 12, 35, 150));
+            var rate = shipment.Rates[0];
+            return rate.TotalCharges;
+        }
+
+        /// <summary>
+        /// The get shipping rate.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="decimal"/>.
+        /// </returns>
+        public decimal GetShippingRate2()
+        {
+            var service = new RateService();
+            var package = new InternationalPackage();
+            package.Country = "Russia";
+            package.OriginZip = "11201";
+            package.Pounds = 2;
+            package.Ounces = 1;
+            var request = new IntlRateV2Request { UserId = "852TRADE0543" };
+            service.Get(request);
+        }
+    }
+}
---
+++ TdService.ShopAnyWare.Tests/Shipping/ShippingHelperTests.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ShippingHelperTests.cs" company="Naviam">
+//   Vitali Hatalski. 2013
+// </copyright>
+// <summary>
+//   Defines the ShippingHelperTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TdService.ShopAnyWare.Tests.Shipping
+{
+    using NUnit.Framework;
+
+    using TdService.Infrastructure.Helpers;
+
+    /// <summary>
+    /// The shipping helper tests.
+    /// </summary>
+    [TestFixture]
+    public class ShippingHelperTests
+    {
+        /// <summary>
+        /// The should be able to get shipping rates.
+        /// </summary>
+        [Test]
+        public void ShouldBeAbleToGetShippingRates()
+        {
+            var shipping = new ShippingHelper();
+            var total = shipping.GetShippingRate();
+
+            Assert.That(total, Is.Positive);
+        }
+    }
+}
